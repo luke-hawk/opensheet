3a37e8c70d42960e2aca80c19ba2e7e1f1d3554d
luke-hawk <lucas.habrich@gmx.de>
2021-12-30 12:36:52 +0100

    Allow to listen on port defined by env

--- index.js
+++ index.js
@@ -101,4 +101,5 @@ app.get("/:id/:sheet", async (req, res) => {
   );
 });
 
-app.listen(3000, () => console.log("http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
